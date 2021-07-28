48d1ad825c2c8a345445e1650b1b1aa2e0c6bf22
Dalibor989 <lazic_dalibor@hotmail.com>
2021-07-28 14:15:23 +0200

    Cetvrti zadatak

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,11 +1,15 @@
 <nav>
+    @guest
     <h3><a href="/">Home</a></h3>
     <h3><a href="/register">Register</a></h3>
     <h3><a href="/login">Login</a></h3>
+    @endguest
+    @auth
     <h3>
         <form method="POST" action="/logout">
         @csrf
-            <button class="btn btn-link nav-link" type="submit">Logout</button>
-        </form>
+        <button class="btn btn-link nav-link" type="submit">Logout</button>
+    </form>
     </h3>
+    @endauth
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -5,26 +5,23 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TeamController;
 use App\Http\Controllers\PlayerController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group([
+    'middleware' => 'auth'
+], function() {
+    Route::get('/', [TeamController::class, 'index']);
+    Route::get('/teams/{team}', [TeamController::class, 'show'])->name('show.teams');
+    
+    Route::get('/players/{player}', [PlayerController::class, 'show'])->name('show.players');
 
-Route::get('/', [TeamController::class, 'index']);
-Route::get('/teams/{team}', [TeamController::class, 'show'])->name('show.teams');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
-Route::get('/players/{player}', [PlayerController::class, 'show'])->name('show.players');
-
-Route::get('/register', [AuthController::class, 'getRegisterForm'])->name('show.register');
-Route::post('/register', [AuthController::class, 'register']);
-
-Route::get('/login', [AuthController::class, 'getLoginForm']);
-Route::post('/login', [AuthController::class, 'login']);
-
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::group([
+    'middleware' => 'guest'
+], function() {
+    Route::get('/register', [AuthController::class, 'getRegisterForm'])->name('show.register');
+    Route::post('/register', [AuthController::class, 'register']);
+    
+    Route::get('/login', [AuthController::class, 'getLoginForm']);
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+});
